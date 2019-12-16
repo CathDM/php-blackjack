ea1da8c7686c937f91fad44fac7ab4145b686d3f
unknown <de_meulemeester_c@hotmail.com>
2019-12-16 10:11:04 +0100

    doing practice

--- index.php
+++ index.php
@@ -6,23 +6,87 @@
 
 class User{
     //properties
-    public $userName = 'Cath';
-    public $userMail = 'catherinedm82@gmail.com';
+    public $username = 'Cath';
+    public $email = 'catherinedm82@gmail.com';
+    //method
+    //2 creating a constructor 
+
+    public function addFriend(){
+        //(1.)output the value from dinamic property
+       
+        return " $this->username added a frend";
+
+}
 }
 
     //objects
     $userOne = new User();
     $userTwo = new User();
- //to access the value of the property
-    echo 'the class is'. get_class($userTwo);
-    echo "<br/>\n";
+ //1.to access the value of the property
+    echo $userOne ->username .'<br>';
+    echo $userOne ->email .'<br>';
+    echo $userOne ->addFriend().'<br>';
+ //2.to access the value of the property but set it equal to a value
+    //this part gets confusing because the code follows a serten flow 
+    //but here the function wil be repeted by default with that new info 
+    $userTwo->username = 'Joske';
+    $userTwo->email = 'Joskeboske@gmail.com';
+
+ echo $userTwo ->username .'<br>';
+ echo $userTwo->email .'<br>';
+ echo $userTwo ->addFriend().'<br>';
+
+
+// to get an overview of the properties and values available
+// print_r(get_class_vars('User')); //output the value out of properties
+// print_r(get_class_methods('User'));//output method info used with in the property 
 
     ?>
+<!--voorb 2 http://www.sitemasters.be/tutorials/1/1/607/PHP/OOP_(Object_Oriented_Programming)-->
+
+ <?php
+//property
+class Auto {
+
+    
+    //method
+    public $sKleur ="";
+    public function rijden(){
+        return "De auto die " .$this->getKleur()." is rijdt!";
+    }
+    //method om kleur te benoemen # what the fuck is dees eigenlijk
+    public function setKleur($sKleur){
+        $this->sKleur = $sKleur;
+        return;
+    }
+    //methode om kleur op te halen (echt makes no sense at all ma kom)
+    public function getKleur(){
+        return $this->sKleur;
+    }
+}
 
 
 
+$oAuto = new Auto;
+$oAuto->setKleur("groen");
+echo $oAuto->rijden().'<br>';
 
+?>
+<?php
 
+class Auto {
+    
+    private $sKleur = "";
+    public function setKleuren(){
+       $aValideKleuren = array('groen, rood, blauw, paars, wit , geel, zwart');
+       if(! in_array(strtolower($sKleur),($aValideKleuren)){
+           die("kleur is niet bekend:" .$sKleur");
+       });
+}  
+$this->skleur = $sKleur;
+return
+
+?>
 <html lang="en">
 
 <head>
@@ -36,32 +100,7 @@ class User{
 </head>
 
 <body>
-<!-- Public Class Card
 
-Public Const Jack As Integer = 10
-Public Const Queen As Integer = 11
-Public Const King As Integer = 12
-Public Const Ace As Integer = 13 ' You might also choose 1. Toughie.
-
-Public Property Value As Integer
-Public Property Suit As Suit
-End Class
-
-Public Enum Suit
-Hearts,
-Diamonds,
-Clubs,
-Spades
-End Class 
-Dim deck As New List(Of Card)()
-
-For Each cardSuit In { Suit.Hearts, Suit.Diamonds, Suit.Clubs, Suit.Spades }
-    For cardValue As Integer = 2 To 13
-        deck.Add(New Card() With { .Value = cardValue, .Suit = cardSuit })
-    Next
-Next
-Dim hearts() As Image = { My.Resources.Two_Of_Hearts, My.Resources.Three_Of_Hearts, ..., My.Resources.Ace_Of_Hearts }
--->
 
 </body>
 </html>
